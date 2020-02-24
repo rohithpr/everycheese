3191713391bea61f8f11799616ab91d6da485cf3
Rohith <praroh2@gmail.com>
2020-02-24 12:29:06 +0530

    Fix typo

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,7 +80,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "everycheese.users.apps.UsersConfig",
-    "everycheese.cheeses.apps.CheeseConfig",
+    "everycheese.cheeses.apps.CheesesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
